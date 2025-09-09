3b66f443d4a09591725eccc00cc69637f213394f
felipealarcon264 <felipealarcon264@gmail.com>
2025-09-09 12:01:59 -0300

    Get y set de Contenido

--- PlatformStreaming/src/Contenido.java
+++ PlatformStreaming/src/Contenido.java
@@ -6,4 +6,52 @@ public class Contenido {
     int duracion;
     String idiomaOriginal;
 
+    public String getIdContenido() {
+        return this.idContenido;
+    }
+
+    public void setIdContenido(String idContenido) {
+        this.idContenido = idContenido;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getSinopsis() {
+        return this.sinopsis;
+    }
+
+    public void setSinopsis(String sinopsis) {
+        this.sinopsis = sinopsis;
+    }
+
+    public String getDirector() {
+        return this.director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getDuracion() {
+        return this.duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    public String getIdiomaOriginal() {
+        return this.idiomaOriginal;
+    }
+
+    public void setIdiomaOriginal(String idiomaOriginal) {
+        this.idiomaOriginal = idiomaOriginal;
+    }
+
 }
